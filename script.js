9ea08caccb3c7121925d99daf05be26e1026e007
Bartek Karczmarczyk <bartek.karczmarczyk@gmail.com>
2018-04-13 12:47:07 +0200

    Worked on styling

--- script.js
+++ script.js
@@ -38,12 +38,15 @@ function getUserData() {
             if (data.stream) {
               stream.url = data.stream.channel.url;
               stream.broadcastStatus = 'Live';
+              stream.viewers = data.stream.viewers;
               stream.game = data.stream.game;
               stream.description = data.stream.channel.status;
-            } else {
+            } 
+            else {
               stream.url = '';
               stream.broadcastStatus = 'Offline';
               stream.game = '';
+              stream.viewers = 0;
               stream.description = '';
             }
             console.log(stream);
@@ -59,21 +62,43 @@ $(document).ajaxStop(() => {
   streamData.forEach((stream, index) => {
     let firstColumnAdditionalClass = '';
     if (index % 2 == 0) {
-      htmlToAppend += '<div class="row justify-content-center mb-2">';
-      firstColumnAdditionalClass = ' mr-2';
+      htmlToAppend += '<div class="row justify-content-center">';
     }
-    htmlToAppend += generateStreamCardHTML(stream.display_name, stream.logo, firstColumnAdditionalClass, stream.broadcastStatus);
+    htmlToAppend += generateStreamCardHTML(stream.display_name, stream.logo, stream.viewers, stream.game, stream.description);
     if (index % 2 == 1)
       htmlToAppend += '</div>';
   });
-  $("#streamers-grid").append(htmlToAppend);   
+  
+  $("#streamers-grid").append(htmlToAppend);
+  $("#waiting-div").hide();
+  //$(".streamCard").fadeIn(1000);
+  $(".streamCard").each((index, element) => {
+
+    $(element).delay(index * 200).fadeIn(1000);
+    if (index % 2 == 0) {
+      $(element).addClass('animated fadeInLeft');
+    }
+    else {
+      $(element).addClass('animated fadeInRight');
+    }
+
+  });
 });
 
-function generateStreamCardHTML(streamName, logoURL, additionalClass, broadcastStatus) {
+function generateStreamCardHTML(streamName, logoURL, viewers, game, description) {
+  let status;
+  let streamStatusClass = '';
+  if (viewers != 0) {
+    status = `<img class="viewers-img" src="https://openclipart.org/image/2400px/svg_to_png/202776/pawn.png"> ${viewers} | ${game}`;
+  } 
+  else {
+    status = ' Offline';
+    streamStatusClass = 'offline'; 
+  }
   return `
-    <div class="col-md-4 bg-secondary p-1${additionalClass}">
-      <img class='stream-logo pr-3' src="${logoURL}" alt="logo" align="left">
-      <p class="h5 pt-2">${streamName}  <span class="badge badge-light h6">Status | Game</span></p>
-      <p class="h6">${broadcastStatus}</p>
-    </div>`;
+  <div style="display: none" class="streamCard ${streamStatusClass} text-light col-lg-4 col-md-8 col-sm-10 p-2 mr-2 mb-2">
+    <img class='stream-logo pr-3' src="${logoURL}" alt="logo" align="left">
+    <p class="h5 pt-2">${streamName}&nbsp;<span class="badge badge-light h6">${status}</span></p>
+    <p class="h6">${description}</p>
+  </div>`;
 }
